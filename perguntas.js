a07a555ce3b1895283069961a07a6b3065a43bc2
jeffe499 <144942612+jeffe499@users.noreply.github.com>
2024-09-16 11:56:07 -0300

    jeff

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Parana',
+    'Qual a 16 cidade mais pobre do Parana?',
+    'Pinhal de São Bento.'
+)
+
+criaCartao(
+    'Brasil',
+    'Qual o 2 Estado com a maior educação do Brasil?',
+    'Parana.'
+)
+
+criaCartao(
+    'Terra',
+    'Qual pais com maior indice de roubos no mundo?',
+    'Brasil.'
+)
+
+criaCartao(
+    'Via Láctea',
+    'O planeta mais poluido da Via Láctea?',
+    'Terra'
+)
